73e145fe6d9ed1d7a3602d8c9cad878cb5994e59
nejrabujak <nejra.bujak@stu.ssst.edu.ba>
2022-05-10 10:45:13 +0200

    Deleted old code

--- src/main/java/com/example/project/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.project.repositories;
-import com.example.project.models.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Item, Long> {
-
-}
