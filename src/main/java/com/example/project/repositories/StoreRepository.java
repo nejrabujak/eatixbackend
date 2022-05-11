be3863fb0f3fe6c2f7fe20d5421e3871f7ad491d
nejrabujak <nejra.bujak@stu.ssst.edu.ba>
2022-05-11 22:53:23 +0200

    New

--- src/main/java/com/example/project/repositories/StoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.repositories;
-
-import com.example.project.models.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface StoreRepository extends JpaRepository<Store, Long> {
-
-}
