d0adca1e4696a6bd886ba295aa5276d2fff1e9d6
nejrabujak <nejra.bujak@stu.ssst.edu.ba>
2022-06-05 00:21:33 +0200

    Added

--- src/main/java/com/example/eatix/models/Store.java
+++ src/main/java/com/example/eatix/models/Store.java
@@ -25,6 +25,9 @@ public class Store {
     @Column(name = "email")
     private String email;
 
+    @Column(name = "password")
+    private String password;
+
     @Column(name = "address")
     private String address;
 
